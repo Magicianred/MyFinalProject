5d107af4a9108d35d86d288a1bdb4f7fcb602c91
enesylc98 <enesabib@gmail.com>
2021-01-31 00:07:06 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    // public = bu class'a diğer her katman ulaşsın demek //
+    //short -decimal bak //
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
